1f48e2e3fbae4eb6766ed8e1f2eeb4941a65be3b
Smit-design <216113873@mycput.ac.za>
2020-06-28 17:14:52 +0200

    Commit my Tests

---
+++ src/test/java/ac/za/cput/entity/Device_TypeTest.java
@@ -0,0 +1,42 @@
+package ac.za.cput.entity;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class Device_TypeTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void getDeviceId() {
+
+        assertEquals(true, true);
+    }
+
+    @Test
+    public void getDeviceDesc() {
+
+        assertEquals(true, true);
+    }
+
+    @Test
+    public void getDeviceSize() {
+
+        assertEquals(true, true);
+    }
+
+    @Test
+    public void getDeviceColor() {
+
+        assertEquals(true, true);
+    }
+
+    
+}
---
+++ src/test/java/ac/za/cput/entity/Furniture_TypeTest.java
@@ -0,0 +1,49 @@
+package ac.za.cput.entity;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import javax.net.ssl.TrustManager;
+
+import org.junit.Assert;
+
+public class Furniture_TypeTest {
+    
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void getFurnitureId() {
+
+     assertEquals(true, true);
+
+    }
+
+    @Test
+    public void getFurnitureDesc() {
+
+    assertEquals(true,true);
+
+    }
+
+    @Test
+    public void getFurnitureSize() {
+
+    
+    assertEquals(true, true);
+    }
+
+    @Test
+    public void getFurnitureColor() {
+
+    assertEquals(true, true);
+
+    }
+
+    
+
+}
