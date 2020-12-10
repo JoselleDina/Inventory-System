ae5e84a274679f2195b16530998b4e7e68fa8f85
emilelubangi <58505456+emilelubangi@users.noreply.github.com>
2020-12-10 16:15:16 +0200

    Assignments T2 (5 and 6) rechecked

--- src/main/java/ac/za/cput/factory/equipment/DeviceFactory.java
+++ src/main/java/ac/za/cput/factory/equipment/DeviceFactory.java
@@ -6,19 +6,19 @@ import ac.za.cput.util.GenericHelper;
 
 public class DeviceFactory {
 
-        public static Device createDevice(String deviceTypeId,String name){
-            String deviceId = GenericHelper.generateId();
-            Device device =new Device.Builder()
-                    .setDeviceId(deviceId)
-                    .setDeviceTypeId(deviceTypeId)
-                    .setName(name)
-                    .build();
-            return device;
+    public static Device createDevice(String deviceTypeId,String name){
+        String deviceId = GenericHelper.generateId();
+        Device device =new Device.Builder()
+                .setDeviceId(deviceId)
+                .setDeviceTypeId(deviceTypeId)
+                .setName(name)
+                .build();
+        return device;
 
 
-        }
+    }
 
     public static Device createDevice(String s) {
-            return null;
+        return null;
     }
 }
